4c6d6c21708a80fda8b3c97de9dbc9467da3ea82
Lyn-Dah <edulind99@gmail.com>
2024-07-03 17:12:20 +0000

    add footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
-
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="bg- bg-yellow-300 w-screen h-80">
+      <div className="flex gap-x-32 font-serif  px-56 py-10 left-0 top-0">
+        <div>
+          <h3>Host Events</h3>
+          <p>Publish Your Events</p>
+          <p>Promote Your Events</p>
+          <p>Sell Tickets Online</p>
+        </div>
+
+        <div>
+          <h3>Discover Events</h3>
+          <p>Formal Events</p>
+          <p>Informal Events</p>
+          <p>Past Events</p>
+        </div>
+
+        <div>
+        <h3>Explore More</h3>
+        <p>Contact Us</p>
+        <p>Careers</p>
+        <p>Blog</p>
+        </div>
+        <div></div>
+        <h3>Subscription</h3>
+        <button className="bg-white absolute"></button>
+
+
+
+      </div>
+    </div>
   )
 }
 
