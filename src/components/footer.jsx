44c5d3890dd2bfc1e4b3faea8aeefbe7e7e0eee3
Lyn-Dah <edulind99@gmail.com>
2024-07-04 13:26:04 +0000

    Footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,34 +1,43 @@
 const Footer = () => {
   return (
-    <div className="bg- bg-yellow-300 w-screen h-80">
-      <div className="flex gap-x-32 font-serif  px-56 py-10 left-0 top-0">
+    <div className="bg- bg-yellow-300 w-screen h-85">
+      <div className="flex gap-x-10 place-content-center py-20">
         <div>
-          <h3>Host Events</h3>
+          <h3 className="font-bold text-[20px] mb-3">Host Events</h3>
           <p>Publish Your Events</p>
           <p>Promote Your Events</p>
           <p>Sell Tickets Online</p>
         </div>
 
         <div>
-          <h3>Discover Events</h3>
+          <h3 className="font-bold mb-3 text-[20px]">Discover Events</h3>
           <p>Formal Events</p>
           <p>Informal Events</p>
           <p>Past Events</p>
         </div>
 
         <div>
-        <h3>Explore More</h3>
-        <p>Contact Us</p>
-        <p>Careers</p>
-        <p>Blog</p>
+          <h3 className="font-bold mb-3 text-[20px]">Explore More</h3>
+          <p>Contact Us</p>
+          <p>Careers</p>
+          <p>Blog</p>
         </div>
         <div></div>
-        <h3>Subscription</h3>
-        <button className="bg-white absolute"></button>
+
+        <div className="flex flex-col">
+          <h3 className="font-bold mb-3 text-[20px]">Subscription</h3>
+          <input type="text" className="mb-3" placeholder="Name" />
+          <input type="text" className="mb-3" placeholder="Email" />
+          <button className="bg-white h-8 ">Subscribe</button>
+        </div>
 
 
 
       </div>
+      <span>
+        icons
+      </span>
+
     </div>
   )
 }
